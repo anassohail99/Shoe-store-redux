763154f0177bed481d4f2c350b39600d0cf54825
anassohail99 <anas.sohailabs1@gmail.com>
2020-07-16 00:39:27 +0500

    Refactored

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,22 +7,26 @@ const Main = () => {
       <h3>DESIGN AND DEVELOPED BY ANAS BIN SOHAIL</h3>
       <ul className="icon">
         <li>
-          <a href="https://github.com/anassohail99/" target="_blank">
+          <a
+            href="https://github.com/anassohail99/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
             <i className="fab fa-github" />
           </a>
         </li>
         <li>
-          <a href="#" target="_blank">
+          <a href="#" rel="noopener noreferrer" target="_blank">
             <i className="fab fa-facebook" />
           </a>
         </li>
         <li>
-          <a href="#" target="_blank">
+          <a href="#" rel="noopener noreferrer" target="_blank">
             <i class="fab fa-linkedin" />
           </a>
         </li>
         <li>
-          <a href="#" target="_blank">
+          <a href="#" rel="noopener noreferrer" target="_blank">
             <i className="fab fa-twitter" />
           </a>
         </li>
