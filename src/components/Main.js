c6013246b8f1ad8adec6436e648de0ebf86b5459
anassohail99 <anas.sohailabs1@gmail.com>
2020-07-16 00:01:28 +0500

    Main App Page

---
+++ src/components/Main.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="container content">
+      <h2>SHOE STORE APP</h2>
+      <h3>DESIGN AND DEVELOPED BY ANAS BIN SOHAIL</h3>
+      <ul className="icon">
+        <li>
+          <a href="https://github.com/anassohail99/" target="_blank">
+            <i className="fab fa-github" />
+          </a>
+        </li>
+        <li>
+          <a href="#" target="_blank">
+            <i className="fab fa-facebook" />
+          </a>
+        </li>
+        <li>
+          <a href="#" target="_blank">
+            <i class="fab fa-linkedin" />
+          </a>
+        </li>
+        <li>
+          <a href="#" target="_blank">
+            <i className="fab fa-twitter" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Main;
